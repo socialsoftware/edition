ccf02c572ace8dc17a8ca9665146553f866428bf
António Rito Silva <Rito.Silva@ist.utl.pt>
2015-03-26 00:44:34 +0000

    Small Fixes

--- src/main/java/pt/ist/socialsoftware/edition/utils/search/options/HeteronymSearchOption.java
+++ src/main/java/pt/ist/socialsoftware/edition/utils/search/options/HeteronymSearchOption.java
@@ -9,46 +9,50 @@ import pt.ist.socialsoftware.edition.domain.VirtualEditionInter;
 
 public final class HeteronymSearchOption extends SearchOption {
 
-	private final String xmlID;
+    private final String xmlID;
 
-	public HeteronymSearchOption(@JsonProperty("heteronym") String xmlId) {
-		this.xmlID = xmlId.equals("null") ? null : xmlId;
-	}
+    public HeteronymSearchOption(@JsonProperty("heteronym") String xmlId) {
+        this.xmlID = xmlId.equals("null") ? null : xmlId;
+    }
 
-	@Override
-	public String toString() {
-		return "heteronym:" + xmlID;
-	}
+    @Override
+    public String toString() {
+        return "heteronym:" + xmlID;
+    }
 
-	@Override
-	public boolean visit(SourceInter inter) {
-		return this.isAuthor(inter);
-	}
+    @Override
+    public boolean visit(SourceInter inter) {
+        return this.isAuthor(inter);
+    }
 
-	@Override
-	public boolean visit(VirtualEditionInter inter) {
-		return false;
-	}
+    @Override
+    public boolean visit(VirtualEditionInter inter) {
+        return false;
+    }
 
-	@Override
-	public boolean visit(ExpertEditionInter inter) {
-		return this.isAuthor(inter);
-	}
+    @Override
+    public boolean visit(ExpertEditionInter inter) {
+        return this.isAuthor(inter);
+    }
 
-	private boolean isAuthor(FragInter inter) {
-		if(xmlID == null && inter.getHeteronym().getXmlId() == null) {
-			//Searching for fragments with no authors and fragment has no author
-			return true;
-		} else if(((xmlID != null && inter.getHeteronym().getXmlId() == null) || (xmlID == null && inter.getHeteronym().getXmlId() != null))
-				&& !ALL.equals(xmlID)) {
-			//Searching for fragment with author and fragment has no author or searching for fragment with no author and fragment has author and author can't be all
-			return false;
-		}else{
-			return xmlID.equals(inter.getHeteronym().getXmlId()) || xmlID.equals(ALL);
-		}
-	}
+    private boolean isAuthor(FragInter inter) {
+        if (xmlID == null && inter.getHeteronym().getXmlId() == null) {
+            // Searching for fragments with no authors and fragment has no
+            // author
+            return true;
+        } else if (((xmlID != null && inter.getHeteronym().getXmlId() == null) || (xmlID == null && inter
+                .getHeteronym().getXmlId() != null)) && !ALL.equals(xmlID)) {
+            // Searching for fragment with author and fragment has no author or
+            // searching for fragment with no author and fragment has author and
+            // author can't be all
+            return false;
+        } else {
+            return xmlID.equals(inter.getHeteronym().getXmlId())
+                    || xmlID.equals(ALL);
+        }
+    }
 
-	public boolean hasHeteronym() {
-		return !xmlID.equals(ALL);
-	}
+    public boolean hasHeteronym() {
+        return !xmlID.equals(ALL);
+    }
 }
