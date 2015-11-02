514794d43a4417bdf90952d701048a8f1dc88e94
António Rito Silva <Rito.Silva@ist.utl.pt>
2015-11-02 21:09:45 +0000

    spring boot to deploy in standalone tomcat

--- src/main/java/pt/ist/socialsoftware/edition/Application.java
+++ src/main/java/pt/ist/socialsoftware/edition/Application.java
@@ -1,12 +1,21 @@
 package pt.ist.socialsoftware.edition;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.PropertySource;
 
+import pt.ist.socialsoftware.edition.utils.Bootstrap;
+
+@PropertySource({ "classpath:application.properties" })
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
+public class Application extends SpringBootServletInitializer
+        implements InitializingBean {
 
     @Override
     protected SpringApplicationBuilder configure(
@@ -16,6 +25,11 @@ public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        Bootstrap.initDatabase();
     }
 
 }
