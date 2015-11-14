5353bd03e4bfcbd1ff0553ba2223a4bbdb6166c2
António Rito Silva <Rito.Silva@ist.utl.pt>
2015-11-14 16:17:08 +0000

    google social login

--- src/main/java/pt/ist/socialsoftware/edition/config/Application.java
+++ src/main/java/pt/ist/socialsoftware/edition/config/Application.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.social.SocialWebAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
@@ -15,32 +16,66 @@ import org.springframework.social.connect.web.SignInAdapter;
 import pt.ist.socialsoftware.edition.security.LdoDSignInAdapter;
 import pt.ist.socialsoftware.edition.utils.Bootstrap;
 
-@PropertySource({ "classpath:application.properties",
-        "classpath:secrete.properties" })
+@PropertySource({ "classpath:application.properties", "classpath:secrete.properties" })
 @ComponentScan(basePackages = "pt.ist.socialsoftware.edition")
 @SpringBootApplication
-@EnableAutoConfiguration // (exclude = { DataSourceAutoConfiguration.class })
-public class Application extends SpringBootServletInitializer
-        implements InitializingBean {
+@EnableAutoConfiguration(exclude = { SocialWebAutoConfiguration.class }) //
+// (exclude
+// =
+// {
+// DataSourceAutoConfiguration.class
+// })
+public class Application extends SpringBootServletInitializer implements InitializingBean {
 
-    @Override
-    protected SpringApplicationBuilder configure(
-            SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
 
-    @Override
-    public void afterPropertiesSet() {
-        Bootstrap.initDatabase();
-    }
+	@Override
+	public void afterPropertiesSet() {
+		Bootstrap.initDatabase();
+	}
 
-    @Bean
-    public SignInAdapter signInAdapter() {
-        return new LdoDSignInAdapter(new HttpSessionRequestCache());
-    }
+	@Bean
+	public SignInAdapter signInAdapter() {
+		return new LdoDSignInAdapter(new HttpSessionRequestCache());
+	}
+
+	// @Bean // (destroyMethod = "shutdown")
+	// public DataSource dataSource() {
+	// DriverManagerDataSource mysqldataSource = new DriverManagerDataSource();
+	// mysqldataSource.setDriverClassName("com.mysql.jdbc.Driver");
+	// mysqldataSource.setUrl("jdbc:mysql://localhost:3306/edition_social");
+	// mysqldataSource.setUsername("root");
+	// mysqldataSource.setPassword("");
+	// return mysqldataSource;
+	// }
+
+	// @Bean
+	// public PlatformTransactionManager transactionManager() {
+	// return new DataSourceTransactionManager(dataSource());
+	// }
+	//
+	// @Bean
+	// public DataSourceInitializer dataSourceInitializer(
+	// final DataSource dataSource) {
+	// final DataSourceInitializer initializer = new DataSourceInitializer();
+	// initializer.setDataSource(dataSource);
+	// initializer.setDatabasePopulator(databasePopulator());
+	// return initializer;
+	// }
+	//
+	// private DatabasePopulator databasePopulator() {
+	// ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+	// populator.addScript(
+	// new ClassPathResource("JdbcUsersConnectionRepository.sql",
+	// JdbcUsersConnectionRepository.class));
+	// return populator;
+	// }
 
 }
--- src/main/java/pt/ist/socialsoftware/edition/controller/SignupController.java
+++ src/main/java/pt/ist/socialsoftware/edition/controller/SignupController.java
@@ -26,79 +26,69 @@ import pt.ist.socialsoftware.edition.shared.exception.LdoDException;
 
 @Controller
 public class SignupController {
-    private static Logger log = LoggerFactory.getLogger(SignupController.class);
+	private static Logger log = LoggerFactory.getLogger(SignupController.class);
 
-    @Inject
-    private PasswordEncoder passwordEncoder;
+	@Inject
+	private PasswordEncoder passwordEncoder;
 
-    private final ProviderSignInUtils providerSignInUtils;
+	private final ProviderSignInUtils providerSignInUtils;
 
-    @Inject
-    public SignupController(ConnectionFactoryLocator connectionFactoryLocator,
-            UsersConnectionRepository connectionRepository) {
-        this.providerSignInUtils = new ProviderSignInUtils(
-                connectionFactoryLocator, connectionRepository);
-    }
+	@Inject
+	public SignupController(ConnectionFactoryLocator connectionFactoryLocator,
+			UsersConnectionRepository connectionRepository) {
+		this.providerSignInUtils = new ProviderSignInUtils(connectionFactoryLocator, connectionRepository);
+	}
 
-    @RequestMapping(value = "/signup", method = RequestMethod.GET)
-    public SignupForm signupForm(WebRequest request) {
-        log.debug("signupForm");
+	@RequestMapping(value = "/signup", method = RequestMethod.GET)
+	public SignupForm signupForm(WebRequest request) {
+		log.debug("signupForm");
 
-        Connection<?> connection = providerSignInUtils
-                .getConnectionFromSession(request);
-        if (connection != null) {
-            request.setAttribute("message",
-                    "Your " + StringUtils
-                            .capitalize(connection.getKey().getProviderId())
-                    + " account is not associated with a LdoD Archive account. If you\'re new, please sign up.",
-                    // new Message(MessageType.INFO,
-                    // "Your " + StringUtils.capitalize(
-                    // connection.getKey().getProviderId())
-                    // + " account is not associated with a LdoD Archive
-                    // account. If you're new, please sign up."),
-                    WebRequest.SCOPE_REQUEST);
+		Connection<?> connection = providerSignInUtils.getConnectionFromSession(request);
+		if (connection != null) {
+			request.setAttribute("message",
+					"Your " + StringUtils.capitalize(connection.getKey().getProviderId())
+							+ " account is not associated with a LdoD Archive account. If you\'re new, please sign up.",
+					// new Message(MessageType.INFO,
+					// "Your " + StringUtils.capitalize(
+					// connection.getKey().getProviderId())
+					// + " account is not associated with a LdoD Archive
+					// account. If you're new, please sign up."),
+					WebRequest.SCOPE_REQUEST);
 
-            return SignupForm.fromProviderUser(connection.fetchUserProfile(),
-                    connection.getKey().getProviderId());
-        } else {
-            return new SignupForm();
-        }
-    }
+			return SignupForm.fromProviderUser(connection.fetchUserProfile(), connection.getKey().getProviderId());
+		} else {
+			return new SignupForm();
+		}
+	}
 
-    @RequestMapping(value = "/signup", method = RequestMethod.POST)
-    public String signup(@Valid SignupForm form, BindingResult formBinding,
-            WebRequest request) {
-        log.debug(
-                "signup username:{}, firstName:{}, lastName:{}, email:{}, socialMedia:{}, socialId:{}",
-                form.getUsername(), form.getFirstName(), form.getLastName(),
-                form.getEmail(), form.getSocialMediaService(),
-                form.getSocialMediaId());
+	@RequestMapping(value = "/signup", method = RequestMethod.POST)
+	public String signup(@Valid SignupForm form, BindingResult formBinding, WebRequest request) {
+		log.debug("signup username:{}, firstName:{}, lastName:{}, email:{}, socialMedia:{}, socialId:{}",
+				form.getUsername(), form.getFirstName(), form.getLastName(), form.getEmail(),
+				form.getSocialMediaService(), form.getSocialMediaId());
 
-        if (formBinding.hasErrors()) {
-            return null;
-        }
+		if (formBinding.hasErrors()) {
+			return null;
+		}
 
-        SocialMediaService socialMediaService = form
-                .getSocialMediaService() != "" ? SocialMediaService.valueOf(
-                        form.getSocialMediaService().toUpperCase()) : null;
+		SocialMediaService socialMediaService = form.getSocialMediaService() != ""
+				? SocialMediaService.valueOf(form.getSocialMediaService().toUpperCase()) : null;
 
-        LdoDUser user = null;
-        try {
-            user = LdoD.getInstance().createUser(passwordEncoder,
-                    form.getUsername(), form.getPassword(), form.getFirstName(),
-                    form.getLastName(), form.getEmail(), socialMediaService,
-                    form.getSocialMediaId());
-        } catch (LdoDException e) {
-            formBinding.rejectValue("username", "user.duplicateUsername",
-                    "already in use");
-        }
+		LdoDUser user = null;
+		try {
+			user = LdoD.getInstance().createUser(passwordEncoder, form.getUsername(), form.getPassword(),
+					form.getFirstName(), form.getLastName(), form.getEmail(), socialMediaService,
+					form.getSocialMediaId());
+		} catch (LdoDException e) {
+			formBinding.rejectValue("username", "user.duplicateUsername", "already in use");
+		}
 
-        if (user != null) {
-            SigninUtils.signin(request, user);
-            providerSignInUtils.doPostSignUp(user.getUsername(), request);
-            return "redirect:/";
-        }
-        return null;
-    }
+		if (user != null) {
+			SigninUtils.signin(request, user);
+			providerSignInUtils.doPostSignUp(user.getUsername(), request);
+			return "redirect:/";
+		}
+		return null;
+	}
 
 }
