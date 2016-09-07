3fb89f3dd0bde01d3d2b7e0dbd938f4f2e198849
António Rito Silva <Rito.Silva@ist.utl.pt>
2016-09-07 15:08:49 +0100

    refactor DateProperty in recommendations

--- src/main/java/pt/ist/socialsoftware/edition/recommendation/properties/DateProperty.java
+++ src/main/java/pt/ist/socialsoftware/edition/recommendation/properties/DateProperty.java
@@ -2,16 +2,15 @@ package pt.ist.socialsoftware.edition.recommendation.properties;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import pt.ist.socialsoftware.edition.domain.ExpertEditionInter;
 import pt.ist.socialsoftware.edition.domain.FragInter;
 import pt.ist.socialsoftware.edition.domain.Fragment;
-import pt.ist.socialsoftware.edition.domain.LdoD;
 import pt.ist.socialsoftware.edition.domain.ManuscriptSource;
 import pt.ist.socialsoftware.edition.domain.PrintedSource;
 import pt.ist.socialsoftware.edition.domain.RecommendationWeights;
@@ -20,65 +19,11 @@ import pt.ist.socialsoftware.edition.domain.Source.SourceType;
 import pt.ist.socialsoftware.edition.domain.SourceInter;
 
 public class DateProperty extends StorableProperty {
-	private static Integer STARTYEAR = 1900;
-	private static Integer ENDYEAR = 1940;
-
-	private static Integer getEndYear() {
-		if (ENDYEAR == null) {
-			getYears();
-		}
-		return ENDYEAR;
-	}
+	private static Integer STARTYEAR = 1913;
+	private static Integer ENDYEAR = 1934;
 
 	private static int getNumberOfYears() {
-		if (getEndYear() != null && getStartYear() != null) {
-			return 1 + getEndYear() - getStartYear();
-		} else
-			return 0;
-	}
-
-	private static Integer getStartYear() {
-		if (STARTYEAR == null) {
-			getYears();
-		}
-		return STARTYEAR;
-	}
-
-	private static void getYears() {
-		TreeSet<Integer> years = new TreeSet<Integer>();
-		for (Fragment fragment : LdoD.getInstance().getFragmentsSet()) {
-			for (FragInter inter : fragment.getFragmentInterSet()) {
-				if (inter.getLdoDDate() != null) {
-					years.add(inter.getLdoDDate().getDate().getYear());
-				}
-			}
-			for (Source source : fragment.getSourcesSet()) {
-				if (source.getType().equals(SourceType.MANUSCRIPT)) {
-					ManuscriptSource manu = (ManuscriptSource) source;
-					if (manu.getLdoDDate() != null) {
-						years.add(manu.getLdoDDate().getDate().getYear());
-					}
-					for (SourceInter inter : manu.getSourceIntersSet())
-						if (inter.getLdoDDate() != null) {
-							years.add(inter.getLdoDDate().getDate().getYear());
-						}
-				} else if (source.getType().equals(SourceType.PRINTED)) {
-					PrintedSource printed = (PrintedSource) source;
-					if (printed.getLdoDDate() != null) {
-						years.add(printed.getLdoDDate().getDate().getYear());
-					}
-					for (SourceInter inter : printed.getSourceIntersSet())
-						if (inter.getLdoDDate() != null) {
-							years.add(inter.getLdoDDate().getDate().getYear());
-						}
-				}
-			}
-		}
-
-		if (years.size() > 0) {
-			STARTYEAR = years.first();
-			ENDYEAR = years.last();
-		}
+		return ENDYEAR - STARTYEAR + 1;
 	}
 
 	public DateProperty() {
@@ -94,7 +39,7 @@ public class DateProperty extends StorableProperty {
 	}
 
 	private List<Double> buildVector(int date, List<Double> vector) {
-		int start = date - getStartYear();
+		int start = date - STARTYEAR;
 		// double degree = 1.0 / vector.size();
 		double degree = 0.1;
 		double j = 1.0;
@@ -116,17 +61,8 @@ public class DateProperty extends StorableProperty {
 	}
 
 	@Override
-	public List<Double> extractVector(ExpertEditionInter expertEditionInter) {
-		List<Double> vector = new ArrayList<Double>(getDefaultVector());
-		if (expertEditionInter.getLdoDDate() != null) {
-			vector = buildVector(expertEditionInter.getLdoDDate().getDate().getYear(), vector);
-		}
-		return vector;
-	}
-
-	@Override
 	public List<Double> extractVector(Fragment fragment) {
-		Set<Integer> dates = new TreeSet<Integer>();
+		Set<Integer> dates = new HashSet<Integer>();
 		for (FragInter inter : fragment.getFragmentInterSet()) {
 			if (inter.getLdoDDate() != null) {
 				dates.add(inter.getLdoDDate().getDate().getYear());
@@ -137,43 +73,32 @@ public class DateProperty extends StorableProperty {
 				dates.add(source.getLdoDDate().getDate().getYear());
 			}
 		}
-		List<Double> vector = new ArrayList<Double>(getDefaultVector());
+		List<Double> vector = getDefaultVector();
 		vector = buildVector(dates, vector);
 		return vector;
 	}
 
 	@Override
 	public List<Double> extractVector(Source source) {
-		Set<Integer> dates = new TreeSet<Integer>();
-		if (source.getType().equals(SourceType.MANUSCRIPT)) {
-			ManuscriptSource manu = (ManuscriptSource) source;
-			if (manu.getLdoDDate() != null) {
-				dates.add(manu.getLdoDDate().getDate().getYear());
-			}
-			for (SourceInter inter : manu.getSourceIntersSet()) {
-				if (inter.getLdoDDate() != null) {
-					dates.add(inter.getLdoDDate().getDate().getYear());
-				}
-			}
-		} else if (source.getType().equals(SourceType.PRINTED)) {
-			PrintedSource printed = (PrintedSource) source;
-			if (printed.getLdoDDate() != null) {
-				dates.add(printed.getLdoDDate().getDate().getYear());
-			}
-			for (SourceInter inter : printed.getSourceIntersSet()) {
-				if (inter.getLdoDDate() != null) {
-					dates.add(inter.getLdoDDate().getDate().getYear());
-				}
+		Set<Integer> dates = new HashSet<Integer>();
+		if (source.getLdoDDate() != null) {
+			dates.add(source.getLdoDDate().getDate().getYear());
+		}
+
+		for (SourceInter sourceInter : source.getSourceIntersSet()) {
+			if (sourceInter.getLdoDDate() != null) {
+				dates.add(sourceInter.getLdoDDate().getDate().getYear());
 			}
 		}
-		List<Double> vector = new ArrayList<Double>(getDefaultVector());
+
+		List<Double> vector = getDefaultVector();
 		vector = buildVector(dates, vector);
 		return vector;
 	}
 
 	@Override
 	protected List<Double> extractVector(SourceInter sourceInter) {
-		List<Double> vector = new ArrayList<Double>(getDefaultVector());
+		List<Double> vector = getDefaultVector();
 		if (sourceInter.getLdoDDate() != null)
 			vector = buildVector(sourceInter.getLdoDDate().getDate().getYear(), vector);
 		return vector;
