1e1883581ccc7b9cee1385415c3436230190c0d2
António Rito Silva <Rito.Silva@ist.utl.pt>
2017-12-20 11:26:08 +0000

    fix

--- src/main/java/pt/ist/socialsoftware/edition/controller/FragmentController.java
+++ src/main/java/pt/ist/socialsoftware/edition/controller/FragmentController.java
@@ -15,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -129,6 +130,12 @@ public class FragmentController {
 		model.addAttribute("writer", writer);
 
 		return "fragment/main";
+	}
+
+	// added until the controller below is removed
+	@ExceptionHandler({ Exception.class })
+	public String handleException(Exception ex) {
+		return "redirect:/error";
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "/fragment/inter/{externalId}")
