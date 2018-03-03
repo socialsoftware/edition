e39d8fed109c0fb3ac2b963e04a81435143f086a
Gon19 <goncalo.m.marques@tecnico.ulisboa.pt>
2018-03-03 19:11:39 +0000

    Redid the setup with the core changes

---
+++ edition-core/src/main/java/pt/ist/socialsoftware/edition/core/social/facebook/FacebookController.java
@@ -0,0 +1,63 @@
+package pt.ist.socialsoftware.edition.core.social.facebook;
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.social.connect.Connection;
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.User;
+import org.springframework.social.facebook.api.impl.FacebookTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import pt.ist.socialsoftware.edition.core.controller.AdminController;
+import pt.ist.socialsoftware.edition.core.domain.LdoD;
+import pt.ist.socialsoftware.edition.core.session.LdoDSession;
+
+@Controller
+@RequestMapping(value="/social/facebook")
+public class FacebookController {
+    private static Logger logger = LoggerFactory.getLogger(FacebookController.class);
+
+
+    @Inject
+    private Facebook facebook;
+    private User user;
+
+    @RequestMapping(value="/home", method=RequestMethod.GET)
+    public String showHome(Model model) {
+        logger.debug("FB home");
+        return "social/facebook/home";
+    }
+
+    @RequestMapping(value="/user", method=RequestMethod.POST)
+    public String getFacebookUser(Model model, @RequestParam("message") String message) {
+        logger.debug("getFBUser");
+        user = facebook.userOperations().getUserProfile();
+        String uID = user.getId();
+        logger.debug(user.getName());
+        //facebook.feedOperations().post(uID, message);
+        return "/social/facebook/success";
+    }
+
+   /* @RequestMapping(value="/home", method=RequestMethod.GET)
+    public String showFeed(Model model) {
+        model.addAttribute("feed", fb.feedOperations().getFeed());
+        return "social/facebook/home";
+    }*/
+
+    @RequestMapping(value="/feed", method=RequestMethod.POST)
+    public String postUpdate(String message) {
+        facebook.feedOperations().updateStatus(message);
+        return "redirect:/facebook/home";
+    }
+
+
+}
