ecb87c990eebaff71396c9c5063153928df1db47
Miguel Cruz <miguelsantoscruz95@gmail.com>
2018-03-20 18:37:29 +0000

    Complete header with links

--- src/about.js
+++ src/about.js
@@ -1,1 +1,49 @@
 import React from 'react';
+import ReactHTMLParser from 'react-html-parser';
+
+
+export default class About extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            html: '',
+        };
+    }
+
+    componentDidMount() {
+        fetch(`http://1.1.1.10:8080/about/${this.props.url}`)
+            .then(res => res.text())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        // The response is an array with only 1 element, the element body
+                        html: ReactHTMLParser(result)[0],
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error,
+                    });
+                });
+    }
+
+    render() {
+        const { error, isLoaded, html } = this.state;
+        if (this.state.error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        }
+        return (
+            <div className={'container ldod-default'}>
+                {/* The response comes with entire body. props.children removes the element body */}
+                {html.props.children}
+            </div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import { NavDropdown, MenuItem } from 'react-bootstrap';
 import ReactDOM from 'react-dom';
 import HomePage from './home';
+import About from './about';
 import './resources/css/style.css';
 import './resources/css/ldod.css';
 import './resources/css/font-awesome.min.css';
@@ -29,12 +30,19 @@ function TopBarElement(props) {
     if (!props.subsections) {
         return (
             <li>
-                <a>{props.title}</a>
+                <Link to={props.baseLink}>{props.title}</Link>
             </li>
         );
     }
+    // This happens because of Documents menu
+    let baseLink = '';
+    if (props.baseLink) {
+        baseLink = `/${props.baseLink}`;
+    }
 
-    const subsecs = props.subsections.map(subsection => <MenuItem>{subsection}</MenuItem>);
+    const subsecs = props.subsections.map(subsection =>
+        <MenuItem href={`${baseLink}/${subsection.link}`}>{subsection.title}</MenuItem>);
+    console.log(subsecs);
 
     if (props.division) {
         subsecs.splice(props.division, 0, <MenuItem divider />);
@@ -54,39 +62,55 @@ function TopBarList() {
             <div className={'collapse navbar-collapse'}>
                 <ul className={'nav navbar-nav navbar-nav-flex'}>
                     {/* About */}
-                    <li><Link to={'/about'}>about</Link></li>
                     <TopBarElement
                         title={'Acerca'}
-                        subsections={['Arquivo LdoD',
-                            'Vídeos',
-                            'Perguntas Frequentes',
-                            'Codificação de Texto',
-                            'Bibliografia',
-                            'Código de Conduta',
-                            'Política de Privacidade',
-                            'Equipa Editorial',
-                            'Agradecimentos',
-                            'Contacto',
-                            'Copyright']} />
+                        baseLink={'about'}
+                        subsections={[
+                            { title: 'Arquivo LdoD', link: 'archive' },
+                            { title: 'Vídeos', link: 'videos' },
+                            { title: 'Preguntas Frequentes', link: 'faq' },
+                            { title: 'Codificação de Texto', link: 'encoding' },
+                            { title: 'Bibliografia', link: 'articles' },
+                            { title: 'Código de Conduta', link: 'conduct' },
+                            { title: 'Política de Privacidade', link: 'privacy' },
+                            { title: 'Equipa Editorial', link: 'team' },
+                            { title: 'Agradecimentos', link: 'acknowledgements' },
+                            { title: 'Contacto', link: 'contact' },
+                            { title: 'Copyright', link: 'copyright' },
+                        ]} />
                     {/* Reading */}
-                    <TopBarElement title={'Leitura'} />
+                    <TopBarElement title={'Leitura'} baseLink={'reading'} />
                     {/* Documents */}
-                    <TopBarElement title={'Documentos'} subsections={['Testemunhos', 'Fragmentos Codificados']} />
+                    <TopBarElement
+                        title={'Documentos'}
+                        subsections={[
+                            { title: 'Testemunhos', link: 'source/list' },
+                            { title: 'Fragmentos Codificados', link: 'fragments' },
+                        ]} />
                     {/* Editions */}
                     <TopBarElement
                         title={'Edições'}
-                        subsections={['Jacinto do Prado Coelho',
-                            'Teresa Sobral Cunha',
-                            'Richard Zenith',
-                            'Jerónimo Pizarro',
-                            'Arquivo LdoD',
-                            'LdoD-JPC-ANOT',
-                            'LdoD-MALLET']}
+                        baseLink={'edition'}
+                        subsections={[
+                            { title: 'Jacinto do Prado Coelho', link: 'acronym/JPC' },
+                            { title: 'Teresa Sobral Cunha', link: 'acronym/TSC' },
+                            { title: 'Richard Zenith', link: 'acronym/RZ' },
+                            { title: 'Jerónimo Pizarro', link: 'acronym/JP' },
+                            { title: 'Arquivo LdoD', link: 'acronym/LdoD-Arquivo' },
+                            { title: 'LdoD-JPC-ANOT', link: 'acronym/JPC-anot' },
+                            { title: 'LdoD-MALLET', link: 'acronym/LdoD-Mallet' },
+                        ]}
                         division={5} />
                     {/* Search */}
-                    <TopBarElement title={'Pesquisa'} subsections={['Pesquisa Simples', 'Pesquisa Avançada']} />
+                    <TopBarElement
+                        title={'Pesquisa'}
+                        baseLink={'search'}
+                        subsections={[
+                            { title: 'Pesquisa Simples', link: 'simple' },
+                            { title: 'Pesquisa Avançada', link: 'advanced' },
+                        ]} />
                     {/* Virtual */}
-                    <TopBarElement title={'Virtual'} />
+                    <TopBarElement title={'Virtual'} baseLink={'virtualeditions'} />
                     {/* Lang */}
                     <li className={'nav-lang'} >
                         <a className={'active'}>PT</a>
@@ -113,9 +137,11 @@ function App() {
         <Router>
             <div>
                 <TopBar />
-                <Route path={'/'} component={HomePage} />
+                <Switch>
+                    <Route path={'/about/archive'} render={() => <About url={'archive'} />} />
+                    <Route path={'/'} component={HomePage} />
+                </Switch>
             </div>
-            {/* <Route path={'/about'} component={About} /> */}
         </Router>
     );
 }
