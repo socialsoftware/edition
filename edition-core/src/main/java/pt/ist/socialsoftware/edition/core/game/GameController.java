441eccb6f86291318f448ec6b5bea78c6147abc3
Gon19 <goncalo.m.marques@tecnico.ulisboa.pt>
2018-05-08 12:33:46 +0100

    Added filter that allows CORS

--- edition-core/src/main/java/pt/ist/socialsoftware/edition/core/game/GameController.java
+++ edition-core/src/main/java/pt/ist/socialsoftware/edition/core/game/GameController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.*;
+import pt.ist.socialsoftware.edition.core.domain.LdoDUser;
 
 @RestController
 public class GameController {
@@ -15,6 +16,7 @@ public class GameController {
 	@CrossOrigin(origins = "http://localhost:3000")
 	@RequestMapping(method = RequestMethod.GET, value = "/game")
 	public Game greeting(@RequestParam(required=true, defaultValue="DefaultUser") String username, @RequestParam(required=true, defaultValue="DefaultVirtualEdition") String virtualEdition) {
-		return new Game(counter.incrementAndGet(),"teste", username, virtualEdition);
+		log.debug("AQUI --" + LdoDUser.getAuthenticatedUser().getUsername());
+		return new Game(counter.incrementAndGet(),"teste", LdoDUser.getAuthenticatedUser().getUsername(), virtualEdition);
 	}
 }
