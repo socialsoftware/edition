772d644fc773fe179c4662de825f78d74ce006e3
António Rito Silva <Rito.Silva@ist.utl.pt>
2018-06-01 20:45:49 +0100

    divde into frontend and backend filter chains

--- edition-core/src/main/java/pt/ist/socialsoftware/edition/core/config/WebSecurityConfig.java
+++ edition-core/src/main/java/pt/ist/socialsoftware/edition/core/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -13,6 +14,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -33,37 +35,66 @@ import pt.ist.socialsoftware.edition.core.security.LdoDUserDetailsService;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 	private static Logger log = LoggerFactory.getLogger(WebSecurityConfig.class);
 
 	@Inject
 	Environment environment;
 
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/resources/**");
+	@Configuration
+	@Order(1)
+	public static class BackendConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		public void configure(WebSecurity web) throws Exception {
+			web.ignoring().antMatchers("/resources/**");
+		}
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			log.debug("configure");
+
+			// to make accessible for unregistered users comment
+			// .anyRequest().authenticated() after .antMatchers("/", "/auth/**",
+			// "/signin/**", "/signup/**").permitAll()
+
+			http.csrf().disable().formLogin().loginPage("/signin").successHandler(ldoDAuthenticationSuccessHandler())
+					.loginProcessingUrl("/signin/authenticate").failureUrl("/signin?param.error=bad_credentials").and()
+					.logout().logoutUrl("/signout").deleteCookies("JSESSIONID").invalidateHttpSession(true).and()
+					.authorizeRequests().antMatchers("/virtualeditions/restricted/**", "/user/**").authenticated()
+					.antMatchers("/admin/**").hasAuthority(RoleType.ROLE_ADMIN.name()).and().sessionManagement()
+					.maximumSessions(2).sessionRegistry(sessionRegistry());
+		}
+
+		@Bean
+		public SessionRegistry sessionRegistry() {
+			return new SessionRegistryImpl();
+		}
+
+		@Bean
+		public LdoDAuthenticationSuccessHandler ldoDAuthenticationSuccessHandler() {
+			return new LdoDAuthenticationSuccessHandler();
+		}
+
 	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		log.debug("configure");
+	@Configuration
+	@Order(2)
+	public static class FrontendConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		public void configure(WebSecurity web) throws Exception {
+			web.ignoring().antMatchers("/resources/**");
+		}
 
-		// to make accessible for unregistered users comment
-		// .anyRequest().authenticated() after .antMatchers("/", "/auth/**",
-		// "/signin/**", "/signup/**").permitAll()
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			log.debug("configure");
 
-		http.csrf().disable().formLogin().loginPage("/signin").successHandler(ldoDAuthenticationSuccessHandler())
-				.loginProcessingUrl("/signin/authenticate").failureUrl("/signin?param.error=bad_credentials").and()
-				.logout().logoutUrl("/signout").deleteCookies("JSESSIONID").invalidateHttpSession(true).and()
-				.authorizeRequests().antMatchers("/virtualeditions/restricted/**", "/user/**", "/api/services/**")
-				.authenticated().antMatchers("/admin/**").hasAuthority(RoleType.ROLE_ADMIN.name()).and()
-				.sessionManagement().maximumSessions(2).sessionRegistry(sessionRegistry());
+			http.authorizeRequests().antMatchers("/api/services/**").authenticated().and()
+					.addFilter(new JWTAuthenticationFilter(authenticationManager()))
+					.addFilter(new JWTAuthorizationFilter(authenticationManager())).sessionManagement()
+					.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-		http.addFilter(new JWTAuthenticationFilter(authenticationManager()))
-				.addFilter(new JWTAuthorizationFilter(authenticationManager()));
-		// this disables session creation on Spring Security
-		// .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
+		}
 	}
 
 	@Inject
@@ -78,11 +109,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
 		return source;
-	}
-
-	@Bean
-	public SessionRegistry sessionRegistry() {
-		return new SessionRegistryImpl();
 	}
 
 	@Bean
@@ -106,11 +132,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	public LdoDSocialUserDetailsService ldoDSocialUserDetailsService() {
 		return new LdoDSocialUserDetailsService(ldoDUserDetailsService());
-	}
-
-	@Bean
-	public LdoDAuthenticationSuccessHandler ldoDAuthenticationSuccessHandler() {
-		return new LdoDAuthenticationSuccessHandler();
 	}
 
 }
