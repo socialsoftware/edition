1d590019353f47261f1b5f904ccfc2a380d5791b
Gon19 <goncalo.m.marques@tecnico.ulisboa.pt>
2018-06-23 18:02:37 +0100

    added jwt security constants to jwt package

---
+++ edition-ldod/src/main/java/pt/ist/socialsoftware/edition/ldod/security/jwt/JWTSecurityConstants.java
@@ -0,0 +1,8 @@
+package pt.ist.socialsoftware.edition.ldod.security.jwt;
+
+public class JWTSecurityConstants {
+	public static final String SECRET = "SecretKeyToGenJWTs";
+	public static final long EXPIRATION_TIME = 864_000_000; // 10 days
+	public static final String TOKEN_PREFIX = "Bearer ";
+	public static final String HEADER_STRING = "Authorization";
+}
