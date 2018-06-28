4d694a8b1ac2b315796dcf14f0633aa432053a7b
António Rito Silva <Rito.Silva@ist.utl.pt>
2018-06-28 14:00:10 +0100

    fix

--- edition-ldod/src/main/java/pt/ist/socialsoftware/edition/ldod/dto/VirtualEditionInterListDto.java
+++ edition-ldod/src/main/java/pt/ist/socialsoftware/edition/ldod/dto/VirtualEditionInterListDto.java
@@ -7,16 +7,24 @@ import java.util.stream.Collectors;
 import pt.ist.socialsoftware.edition.ldod.domain.VirtualEdition;
 
 public class VirtualEditionInterListDto {
-	List<VirtualEditionInterDto> virtualEditionInterList = new ArrayList<>();
+	private List<VirtualEditionInterDto> virtualEditionInterList = new ArrayList<>();
 
 	public VirtualEditionInterListDto() {
 	}
 
 	public VirtualEditionInterListDto(VirtualEdition virtualEdition) {
-		this.virtualEditionInterList = virtualEdition.getIntersSet().stream()
-				.sorted((i1, i2) -> i2.getNumber() - i1.getNumber())
-				.map(i -> new VirtualEditionInterDto(i.getTitle(), i.getNumber(), i.getUrlId()))
-				.collect(Collectors.toList());
+		this.setVirtualEditionInterList(
+				virtualEdition.getIntersSet().stream().sorted((i1, i2) -> i2.getNumber() - i1.getNumber())
+						.map(i -> new VirtualEditionInterDto(i.getTitle(), i.getNumber(), i.getUrlId()))
+						.collect(Collectors.toList()));
+	}
+
+	public List<VirtualEditionInterDto> getVirtualEditionInterList() {
+		return this.virtualEditionInterList;
+	}
+
+	public void setVirtualEditionInterList(List<VirtualEditionInterDto> virtualEditionInterList) {
+		this.virtualEditionInterList = virtualEditionInterList;
 	}
 
 }
