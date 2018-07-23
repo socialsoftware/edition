c6556f31209ee3eb41f846549faff8beceb4e892
Gon19 <goncalo.m.marques@tecnico.ulisboa.pt>
2018-07-23 18:42:18 +0100

    changes to websockets from the server side

--- edition-ldod/src/main/java/pt/ist/socialsoftware/edition/ldod/controller/api/WebSocketController.java
+++ edition-ldod/src/main/java/pt/ist/socialsoftware/edition/ldod/controller/api/WebSocketController.java
@@ -2,9 +2,12 @@ package pt.ist.socialsoftware.edition.ldod.controller.api;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import pt.ist.socialsoftware.edition.ldod.dto.APIResponse;
@@ -12,13 +15,20 @@ import pt.ist.socialsoftware.edition.ldod.dto.APIResponse;
 @RestController
 public class WebSocketController {
     private static Logger logger = LoggerFactory.getLogger(WebSocketController.class);
+    @Autowired
+    private SimpMessagingTemplate broker;
+
 
     @MessageMapping("/hello")
     @SendTo("/topic/ping")
-    public @ResponseBody
-    APIResponse ping(@Payload String value) {
+    public @ResponseBody APIResponse ping(@Payload String value) {
         logger.debug("client send hello: " + value);
         return new APIResponse(true,"Hello back from the server");
+    }
 
+    @SubscribeMapping("/ping")
+    public void subscribeHandler(){
+        logger.debug("ldod-game handler");
+        //broker.convertAndSend("ldod-game/ping", "Registered");
     }
 }
--- edition-ldod/src/main/java/pt/ist/socialsoftware/edition/ldod/controller/api/WebSocketEventListener.java
+++ edition-ldod/src/main/java/pt/ist/socialsoftware/edition/ldod/controller/api/WebSocketEventListener.java
@@ -2,28 +2,21 @@ package pt.ist.socialsoftware.edition.ldod.controller.api;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
-
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 import org.springframework.web.socket.messaging.SessionSubscribeEvent;
 import org.springframework.web.socket.messaging.SessionUnsubscribeEvent;
-import pt.ist.socialsoftware.edition.ldod.dto.APIResponse;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class WebSocketEventListener {
 
     private static final Logger logger = LoggerFactory.getLogger(WebSocketEventListener.class);
-
-    @Autowired
-    private SimpMessagingTemplate broker;
-    private Map<String, String> subscribedUsers = new HashMap<>();
+    private List<String> subscribedUsers = new ArrayList<>();
 
     @EventListener
     public void handleWebSocketConnectListener(SessionConnectedEvent event) {
@@ -36,11 +29,10 @@ public class WebSocketEventListener {
     @EventListener
     public void handleSubscribe(SessionSubscribeEvent event){
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
-        String userId = headerAccessor.getNativeHeader("userId").get(0);
         String destination = headerAccessor.getDestination();
-        logger.info("Received a subscribe to: {}, with Id: {}, from user: {}.", destination, headerAccessor.getSubscriptionId(), userId);
-        broker.convertAndSend("/topic/ping", new APIResponse(true,"You are subscribed"));
-        subscribedUsers.put(destination, userId);
+        String subId = headerAccessor.getSubscriptionId();
+        logger.info("Received a subscribe to: {}, with subId: {}", destination, subId);
+        subscribedUsers.add(subId);
 
      }
 
@@ -58,11 +50,11 @@ public class WebSocketEventListener {
         logger.info("Web socket disconnected {}", headerAccessor.getSessionId());
     }
 
-    public Map<String, String> getSubscribedUsers() {
+    public List<String> getSubscribedUsers() {
         return subscribedUsers;
     }
 
-    public void setSubscribedUsers(Map<String, String> subscribedUsers) {
+    public void setSubscribedUsers(List<String> subscribedUsers) {
         this.subscribedUsers = subscribedUsers;
     }
 
