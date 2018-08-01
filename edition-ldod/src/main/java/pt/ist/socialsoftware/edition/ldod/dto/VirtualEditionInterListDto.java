2a5f71ab101a4d9f91f810b61ed22d8e65580d90
Gon19 <goncalo.m.marques@tecnico.ulisboa.pt>
2018-08-01 17:58:33 +0100

    changed fragment ordering

--- edition-ldod/src/main/java/pt/ist/socialsoftware/edition/ldod/dto/VirtualEditionInterListDto.java
+++ edition-ldod/src/main/java/pt/ist/socialsoftware/edition/ldod/dto/VirtualEditionInterListDto.java
@@ -20,7 +20,7 @@ public class VirtualEditionInterListDto {
 
 	public VirtualEditionInterListDto(VirtualEdition virtualEdition) {
 		this.setVirtualEditionInterList(
-				virtualEdition.getIntersSet().stream().sorted((i1, i2) -> i2.getNumber() - i1.getNumber())
+				virtualEdition.getIntersSet().stream().sorted()
 						.map(i -> new VirtualEditionInterDto((VirtualEditionInter) virtualEdition.getFragInterByUrlId(i.getUrlId())))
 						.collect(Collectors.toList()));
 		this.setTitle(virtualEdition.getTitle());
