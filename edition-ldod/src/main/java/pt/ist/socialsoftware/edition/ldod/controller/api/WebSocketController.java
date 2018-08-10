8e17b181337a1b5014a1689ada68e07afa58e653
Gon19 <goncalo.m.marques@tecnico.ulisboa.pt>
2018-08-10 17:47:38 +0100

    WebSocketController now holds tags and voting

--- edition-ldod/src/main/java/pt/ist/socialsoftware/edition/ldod/controller/api/WebSocketController.java
+++ edition-ldod/src/main/java/pt/ist/socialsoftware/edition/ldod/controller/api/WebSocketController.java
@@ -1,5 +1,7 @@
 package pt.ist.socialsoftware.edition.ldod.controller.api;
 
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,27 +10,40 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.annotation.SubscribeMapping;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import pt.ist.socialsoftware.edition.ldod.dto.APIResponse;
-import pt.ist.socialsoftware.edition.ldod.security.LdoDUserDetails;
 
-import java.security.Principal;
-import java.util.Map;
+
+import java.util.*;
 
 @RestController
 public class WebSocketController {
     private static Logger logger = LoggerFactory.getLogger(WebSocketController.class);
     @Autowired
     private SimpMessagingTemplate broker;
-
+    private Multimap<Object, Object> tagMap = ArrayListMultimap.create();
+    private Map<String, Integer> votingMap = new HashMap<>();
 
     @MessageMapping("/tags")
     @SendTo("/topic/tags")
     public @ResponseBody void handleTags(@Payload Map<String,String> value) {
-        logger.debug("client sent:{}", value.values());
+        tagMap.put(value.values().toArray()[0], value.values().toArray()[1]);
+        logger.debug("tagMap received: {} {}", tagMap.keySet(), tagMap.values());
+        votingMap.put((String) value.values().toArray()[1], 1);
         broker.convertAndSend("/topic/tags", value.values());
+    }
+
+    @MessageMapping("/votes")
+    @SendTo("/topic/votes")
+    public @ResponseBody void handleVotes(@Payload Map<String,String> value) {
+        String authorId = (String) value.values().toArray()[0];
+        Object response = value.values().toArray()[1];
+        HashMap<String,Integer> votes = (HashMap<String, Integer>) response;
+        int res = votingMap.get(votes.get("tag")) + votes.get("vote");
+        logger.debug("teste {}", res);
+        votingMap.put(String.valueOf(votes.get("tag")), votingMap.get(votes.get("tag")) + votes.get("vote"));
+        logger.debug("votingMap keys: {} values {}", votingMap.keySet(), votingMap.values());
+        broker.convertAndSend("/topic/votes", value.values());
     }
 
     @SubscribeMapping("/ping")
