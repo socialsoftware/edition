17f81b9720fc5ea5636bb8252d1d1cea2533a75b
Gon19 <goncalo.m.marques@tecnico.ulisboa.pt>
2018-09-05 19:42:24 +0100

    client now has an array with seconds suggested for each paragraph according to size of it

--- ldod-game/src/game/Fragment.js
+++ ldod-game/src/game/Fragment.js
@@ -8,6 +8,7 @@ class Fragment extends Component {
         super(props);
         this.state = {
             fragment: { },
+            secondAndText: [],
             text: "",
             paragraphText: [],
             index: 0,
@@ -21,51 +22,67 @@ class Fragment extends Component {
     }
     
     componentDidMount() {
+        var text = this.splitIntoParagraph(this.props.fragment.text).text;
+        var time = this.splitIntoParagraph(this.props.fragment.text).time;
+        console.log(time);
         this.setState({
             fragment: this.props.fragment,
             text: this.props.fragment.text,
-            paragraphText: this.splitIntoParagraph(this.props.fragment.text),
+            paragraphText: text,
+            secondAndText: time,
          });
 
     }
 
     splitIntoParagraph(text){
+        var secondAndTextTemp = [];
         var result = [];
         var paragraph = text.split("<br></p>");
         var testInput = paragraph;
         var regex = /(<([^>]+)>)/ig;
+        var largeInt = 100000000;
 
         // Remove tags and spaces on all text
         for(var i = 0; i < testInput.length; i++){
             testInput[i] = testInput[i].replace(regex, "").trim();
+            var size = testInput[i].length
+            //var temp = size >= 200 ? size/6 : 30;
+            var temp = size >= 200 ? 10 + i/largeInt : 5 + i/largeInt;
+            secondAndTextTemp[i] = temp;
         }  
         
         // Check first paragraph and remove it if matches criteria
         if(testInput[0].includes("L. do D.") || testInput[0].length <= 13){
             paragraph.splice(0,1);
-         }
+            secondAndTextTemp.splice(0,1);
+        }
 
         for (i = 0; i < paragraph.length; i++) {
             if (/\S/.test(paragraph[i])) {
                 result.push(paragraph[i]);
             }
         }
-        return result;
+        
+        var res = { text: result, time: secondAndTextTemp};
+        return res;
     }
 
     nextParagraph(){
+        var currentIndex= this.state.index;
+        var nextIndex = currentIndex+1;
+        
         // First round paragraph increment
-        if(this.state.index < this.state.paragraphText.length - 1){
+        if(this.state.index < this.state.paragraphText.length - 1){ 
             this.setState((prevState) => ({
                 index: prevState.index + 1,
-                seconds: prevState.seconds + 0.0000001,
+                seconds: this.state.secondAndText[nextIndex],
             }));
         }
         // Change to round 2 and reset paragraph order
         else if( this.state.index === this.state.paragraphText.length-1){
             this.setState((prevState) => ({
                 index: 0,
-                seconds: prevState.seconds - 0.0000001,
+                seconds: prevState.secondAndText[0],
                 round: prevState.round + 1,
             }));
         }
@@ -79,7 +96,7 @@ class Fragment extends Component {
                     fragment={this.state.fragment}
                     nextParagraph={this.nextParagraph} 
                     paragraphText={this.state.paragraphText[this.state.index]}
-                    seconds={this.state.seconds} 
+                    seconds={this.state.secondAndText[this.state.index]} 
                     round={this.state.round} 
                     endFragment={this.props.endFragment}/>
                 <div className="div-progress">
