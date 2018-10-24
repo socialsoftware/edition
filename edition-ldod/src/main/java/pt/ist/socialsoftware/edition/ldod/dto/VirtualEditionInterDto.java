8cf108176bbefcaecaddec76aaa8f45fa62176ce
Antonio Rito Silva <Rito.Silva@ist.utl.pt>
2018-10-24 12:59:55 +0100

    add fragmentId to VirtualEditionInterDto

--- edition-ldod/src/main/java/pt/ist/socialsoftware/edition/ldod/dto/VirtualEditionInterDto.java
+++ edition-ldod/src/main/java/pt/ist/socialsoftware/edition/ldod/dto/VirtualEditionInterDto.java
@@ -4,6 +4,7 @@ import pt.ist.socialsoftware.edition.ldod.domain.VirtualEditionInter;
 import pt.ist.socialsoftware.edition.ldod.generators.PlainHtmlWriter4OneInter;
 
 public class VirtualEditionInterDto {
+	private String fragmentId;
 	private String title;
 	private int number;
 	private String urlId;
@@ -12,13 +13,8 @@ public class VirtualEditionInterDto {
 	public VirtualEditionInterDto() {
 	}
 
-	public VirtualEditionInterDto(String title, int number, String urlId) {
-		this.title = title;
-		this.number = number;
-		this.urlId = urlId;
-	}
-
 	public VirtualEditionInterDto(VirtualEditionInter inter) {
+		this.setFragmentId(inter.getFragment().getXmlId());
 		this.title = inter.getTitle();
 		this.number = inter.getNumber();
 		this.urlId = inter.getUrlId();
@@ -27,6 +23,14 @@ public class VirtualEditionInterDto {
 		writer.write(false);
 
 		this.text = writer.getTranscription();
+	}
+
+	public String getFragmentId() {
+		return this.fragmentId;
+	}
+
+	public void setFragmentId(String fragmentId) {
+		this.fragmentId = fragmentId;
 	}
 
 	public String getTitle() {
