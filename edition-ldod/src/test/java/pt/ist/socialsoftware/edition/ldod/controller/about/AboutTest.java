8281159f652722ed7a4bd12b4db4657ef5e05a65
Nuno Gonçalves <nunombg@gmail.com>
2019-01-11 12:11:13 +0000

    Update dependencies to support JUnit 5 based tests

--- edition-ldod/src/test/java/pt/ist/socialsoftware/edition/ldod/controller/about/AboutTest.java
+++ edition-ldod/src/test/java/pt/ist/socialsoftware/edition/ldod/controller/about/AboutTest.java
@@ -6,11 +6,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -20,7 +23,8 @@ import pt.ist.socialsoftware.edition.ldod.controller.AboutController;
 import pt.ist.socialsoftware.edition.ldod.controller.LdoDExceptionHandler;
 import pt.ist.socialsoftware.edition.ldod.filters.TransactionFilter;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = Application.class)
 @AutoConfigureMockMvc
 public class AboutTest {
@@ -30,7 +34,7 @@ public class AboutTest {
 	@InjectMocks
 	private AboutController aboutController;
 
-	@Before
+	@BeforeEach
 	public void setUp() {
 		this.mockMvc = MockMvcBuilders.standaloneSetup(this.aboutController)
 				.setControllerAdvice(new LdoDExceptionHandler()).addFilters(new TransactionFilter()).build();
