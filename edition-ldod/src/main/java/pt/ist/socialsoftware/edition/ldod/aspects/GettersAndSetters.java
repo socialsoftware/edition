2d912cc095dc455551b37e9c6430d550246f9577
berhart <bernardo.andrade@tecnico.ulisboa.pt>
2020-03-21 02:45:24 +0000

    added more filters to aspect

--- edition-ldod/src/main/java/pt/ist/socialsoftware/edition/ldod/aspects/GettersAndSetters.java
+++ edition-ldod/src/main/java/pt/ist/socialsoftware/edition/ldod/aspects/GettersAndSetters.java
@@ -19,32 +19,75 @@ public class GettersAndSetters extends AbstractOperationExecutionAspect {
 		// empty default constructor
 	}
 
+	// GETTERS AND SETTERS
+
+	/**
+	 * This is a pointcut accepting all calls and executions of getters methods (methods which return something and start with {@code get}).
+	 */
+	@Pointcut("execution(public * pt.ist.socialsoftware.edition.ldod.domain.*.get*(..)) || call(public * pt.ist.socialsoftware.edition.ldod.domain.*.get*(..))")
+	public void publicGetters() {}
+
+	@Pointcut("execution(pt.ist.fenixframework..* get*(..)) || call(pt.ist.fenixframework..* get*(..))")
+	public void gettersReturningFenixFrameworkObject() {}
+
+	@Pointcut("publicGetters() || publicSetters()")
+	public void publicGettersAndSetters() {}
+
 	/**
 	 * This is a pointcut accepting all calls and executions of setter methods (methods which return void and start with {@code set}).
 	 */
 	@Pointcut("execution(public void pt.ist.socialsoftware.edition.ldod.domain.*.set*(..)) || call(public void pt.ist.socialsoftware.edition.ldod.domain.*.set*(..))")
-	public void publicSetter() {} // NOPMD (Aspect)
+	public void publicSetters() {}
 
 	@Pointcut("execution(* set*(pt.ist.fenixframework..*, ..)) || call(* set*(pt.ist.fenixframework..*, ..))")
-	public void setterWithFenixFrameworkObjectArgument() {} // NOPMD (Aspect)
+	public void settersWithFenixFrameworkObjectArgument() {}
 
-	/**
-	 * This is a pointcut accepting all calls and executions of getter methods (methods which return something and start with {@code get}).
-	 */
-	@Pointcut("execution(public * pt.ist.socialsoftware.edition.ldod.domain.*.get*(..)) || call(public * pt.ist.socialsoftware.edition.ldod.domain.*.get*(..))")
-	public void publicGetter() {} // NOPMD (Aspect)
+	@Pointcut("!gettersReturningFenixFrameworkObject() && !settersWithFenixFrameworkObjectArgument()")
+	public void noGettersOrSettersWithFenixFramework() {}
 
-	@Pointcut("execution(pt.ist.fenixframework..* get*(..)) || call(pt.ist.fenixframework..* get*(..))")
-	public void getterReturningFenixFrameworkObject() {} // NOPMD (Aspect)
+	// ADDS AND REMOVES
 
-	@Pointcut("publicGetter() || publicSetter()")
-	public void publicGetterAndSetter() {} // NOPMD (Aspect)
+	@Pointcut("execution(public void pt.ist.socialsoftware.edition.ldod.domain.*.add*(..)) || call(public void pt.ist.socialsoftware.edition.ldod.domain.*.add*(..))")
+	public void publicAdds() {}
 
-	@Pointcut("!getterReturningFenixFrameworkObject() && !setterWithFenixFrameworkObjectArgument()")
-	public void noGetterOrSetterWithFenixFramework() {} // NOPMD (Aspect)
+	@Pointcut("execution(public void pt.ist.socialsoftware.edition.ldod.domain.*.remove*(..)) || call(public void pt.ist.socialsoftware.edition.ldod.domain.*.remove*(..))")
+	public void publicRemoves() {}
 
-	@Pointcut("(execution(* *(..)) && publicGetterAndSetter() && !setterWithFenixFrameworkObjectArgument())") 
-	public void monitoredOperation() { // noSetterWithFenixFramework and the only one getter for fenix getDomainObject()
+	@Pointcut("publicAdds() || publicRemoves()")
+	public void publicAddsAndRemoves() {}
+
+	// FENIX FRAMEWORK
+
+	@Pointcut("execution(public * pt.ist.fenixframework.FenixFramework.getDomainObject(..)) || call(public * pt.ist.fenixframework.FenixFramework.getDomainObject(..))")
+	public void fenixFrameworkGetDomainObject() {} // This one isn't used on the domain classes
+
+	@Pointcut("execution(public * pt.ist.fenixframework.FenixFramework.getDomainModel(..)) || call(public * pt.ist.fenixframework.FenixFramework.getDomainModel(..))")
+	public void fenixFrameworkGetDomainModel() {}
+
+	@Pointcut("execution(public * pt.ist.fenixframework.FenixFramework.getDomainRoot(..)) || call(public * pt.ist.fenixframework.FenixFramework.getDomainRoot(..))")
+	public void fenixFrameworkGetDomainRoot() {}
+
+	@Pointcut("fenixFrameworkGetDomainObject() || fenixFrameworkGetDomainModel() || fenixFrameworkGetDomainRoot()")
+	public void fenixFrameworkGetters() {}
+
+	// OTHER CLASSES (AND METHODS) THAT MAY BE IMPORTANT TO CATCH
+
+	@Pointcut("execution(public * pt.ist.fenixframework.backend.jvstmojb.pstm.AbstractDomainObject.getExternalId(..)) || call(public * pt.ist.fenixframework.backend.jvstmojb.pstm.AbstractDomainObject.getExternalId(..))")
+	public void fenixFrameworkAbstractDomainObjectGetExternalId() {}
+
+	@Pointcut("execution(public * pt.ist.fenixframework.backend.jvstmojb.pstm.AbstractDomainObject.deleteDomainMetaObject(..)) || call(public * pt.ist.fenixframework.backend.jvstmojb.pstm.AbstractDomainObject.deleteDomainMetaObject(..))")
+	public void fenixFrameworkAbstractDomainObjectDeleteDomainMetaObject() {} // This one isn't used on the domain classes
+
+	@Pointcut("execution(public * pt.ist.fenixframework.backend.jvstmojb.pstm.AbstractDomainObject.deleteDomainObject(..)) || call(public * pt.ist.fenixframework.backend.jvstmojb.pstm.AbstractDomainObject.deleteDomainObject(..))")
+	public void fenixFrameworkAbstractDomainObjectDeleteDomainObject() {}
+
+	@Pointcut("fenixFrameworkAbstractDomainObjectGetExternalId() || fenixFrameworkAbstractDomainObjectDeleteDomainMetaObject() || fenixFrameworkAbstractDomainObjectDeleteDomainObject()")
+	public void otherMethodsThatMayBeImportant() {}
+
+	// KIEKER METHOD
+
+	@Pointcut("(execution(* *(..)) && publicGettersAndSetters() && noGettersOrSettersWithFenixFramework() || publicAddsAndRemoves() || fenixFrameworkGetters() || otherMethodsThatMayBeImportant())") 
+	public void monitoredOperation() {
 		// Aspect Declaration (MUST be empty)
 	}
 }
