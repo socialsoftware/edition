ddd4286bba2b88e76c4801d9238768172c80aeea
berhart <bernardo.andrade@tecnico.ulisboa.pt>
2020-05-17 01:37:02 +0100

    parser in progress

--- edition-ldod/src/main/resources/kieker-trace-analysis/trace-generation/executionTracesToJson.py
+++ edition-ldod/src/main/resources/kieker-trace-analysis/trace-generation/executionTracesToJson.py
@@ -1,30 +1,91 @@
 from sys import exit, argv
-from typing import List
+from typing import List, Dict, Tuple
 from os import path
+import json
+
+file_content: Dict[str, List[List[str]]] = {};
+current_traceID: str = "-1";
+current_controller_and_method: str = "";
+
+def deleteControllersWithNoAccesses():
+    file_content_copy = file_content.copy() # to avoid RuntimeError: dictionary changed size during iteration
+
+    for controller, accessesList in file_content_copy.items():
+        if (not accessesList):
+            del file_content[controller]
 
 def getTraceID(trace: List[str]) -> str:
     return trace[0][1] # e.g, <5[0,0]
 
-def getController(trace: List[str]) -> str:
+def getControllerAndMethod(trace: List[str]) -> str:
     *everything_else, controller_name, controller_method = trace[2].split(sep='.')
     return controller_name + "." + controller_method
 
+def getClassNameAndMethod(trace: List[str]) -> Tuple[str, str]:
+    *everything_else, class_name, method = trace[2].split(sep='.')
+    return class_name, method
+
+def parseMethod(method: str) -> Tuple[str, str]: # (accessed entity, access type)
+    lowered_case_method = method.lower()
+    # print(method)
+    if (lowered_case_method.startswith("get")):
+        if (lowered_case_method.endswith("set")):
+            return method[3:-4], "R"
+        
+        return method[3:], "R"
+
+    elif (lowered_case_method.startswith("set")):
+        return method[3:], "W"
+    # elif (method.startswith("add")):
+    # elif (method.startswith("remove")):
+    else:
+        exit("[WARNING]: You were not expecting this method " + method + " in the parseMethod function")
+
 def parseExecutionTrace(trace: str):
+    global file_content
+    global current_controller_and_method
+
     if ("TraceId" in trace): # for now, i dont care about lines with a trace id
         return
 
     trace = trace.rstrip() # remove extra new lines
-    print(trace)
+    # print(trace)
 
     split_trace: List[str] = trace.split() # split by white spaces
-    print(split_trace)
+    # print(split_trace)
 
     if (len(split_trace) != 4): # According to Kieker's output, a trace seems to only have traces with 4 sections
         exit("[ERROR]: A trace with more than 4 fields was found.");
     
     # The only interesting parts of trace we want to parse are the 1st one (index 0) and 3rd one (index 2)
-    print("Trace ID: " + getTraceID(split_trace))
-    print("Controller.method: " + getController(split_trace))
+    traceID = getTraceID(split_trace)
+    print("Trace ID: " + traceID)
+
+    global current_traceID
+    if (current_traceID != traceID): # if the previous ID is different from the new one, it means we are in a different trace
+        current_traceID = traceID # and consequently, we need to start by parsing the controller first
+
+        controller_and_method = getControllerAndMethod(split_trace)
+        print("Controller.method: " + controller_and_method)
+
+        file_content[controller_and_method] = []
+
+        current_controller_and_method = controller_and_method
+
+        return
+    
+    else: # if traceID hasn't changed, then we must continue to parse it and add the result to the current controller_and_method
+        class_name, method = getClassNameAndMethod(split_trace)
+
+        if ("_Base" not in class_name):
+            return
+
+        accessed_entity, access_type = parseMethod(method)
+        print("accessed_entity: " + accessed_entity)
+        print("access_type: " + access_type)
+
+        file_content[current_controller_and_method].append([accessed_entity, access_type])
+        
 
 if __name__ == "__main__":
     command_line_args: List[str] = argv
@@ -47,3 +108,6 @@ if __name__ == "__main__":
                 continue
 
             parseExecutionTrace(line)
+
+    # deleteControllersWithNoAccesses()
+    print(json.dumps(file_content, indent = 2))
