78828d2671aae4be954f95896a06dee1c8cc4957
berhart <bernardo.andrade@tecnico.ulisboa.pt>
2020-05-17 14:55:59 +0100

    improved execution trace parser script

--- edition-ldod/src/main/resources/kieker-trace-analysis/trace-generation/executionTracesToJson.py
+++ edition-ldod/src/main/resources/kieker-trace-analysis/trace-generation/executionTracesToJson.py
@@ -2,10 +2,17 @@ from sys import exit, argv
 from typing import List, Dict, Tuple
 from os import path
 import json
+import argparse
+
+verbosity: bool = True;
 
 file_content: Dict[str, List[List[str]]] = {};
 current_traceID: str = "-1";
 current_controller_and_method: str = "";
+
+def myPrint(message: str):
+    if (verbosity):
+        print(message)
 
 def deleteControllersWithNoAccesses():
     file_content_copy = file_content.copy() # to avoid RuntimeError: dictionary changed size during iteration
@@ -25,21 +32,30 @@ def getClassNameAndMethod(trace: List[str]) -> Tuple[str, str]:
     *everything_else, class_name, method = trace[2].split(sep='.')
     return class_name, method
 
-def parseMethod(method: str) -> Tuple[str, str]: # (accessed entity, access type)
+def parseMethod(class_name: str, method: str) -> Tuple[str, str]: # (accessed entity, access type)
+    myPrint("Parsing method: " + method + " of class " + class_name)
+    
     lowered_case_method = method.lower()
-    # print(method)
     if (lowered_case_method.startswith("get")):
         if (lowered_case_method.endswith("set")):
-            return method[3:-4], "R"
+            return method[3:-4], "R" # unfortunately I have to do this hack because I can't obtain for now the functions' return type
         
-        return method[3:], "R"
+        elif (class_name.startswith("DomainRoot") or class_name == "FenixFramework"):
+            return "LdoD", "R"
+
+        elif (class_name.endswith("_Base")):     
+            return class_name[: -len("_Base")], "R" # if it's only a get, then the entity is the class iself
+        
+        else:
+            exit("[WARNING]: You were not expecting this method " + method + "from class " + class_name + " in the parseMethod function")
+
 
     elif (lowered_case_method.startswith("set")):
         return method[3:], "W"
     # elif (method.startswith("add")):
     # elif (method.startswith("remove")):
     else:
-        exit("[WARNING]: You were not expecting this method " + method + " in the parseMethod function")
+        exit("[WARNING]: You were not expecting this method " + method + "from class " + class_name + " in the parseMethod function")
 
 def parseExecutionTrace(trace: str):
     global file_content
@@ -49,24 +65,22 @@ def parseExecutionTrace(trace: str):
         return
 
     trace = trace.rstrip() # remove extra new lines
-    # print(trace)
 
     split_trace: List[str] = trace.split() # split by white spaces
-    # print(split_trace)
 
     if (len(split_trace) != 4): # According to Kieker's output, a trace seems to only have traces with 4 sections
         exit("[ERROR]: A trace with more than 4 fields was found.");
     
     # The only interesting parts of trace we want to parse are the 1st one (index 0) and 3rd one (index 2)
     traceID = getTraceID(split_trace)
-    print("Trace ID: " + traceID)
+    myPrint("Trace ID: " + traceID)
 
     global current_traceID
     if (current_traceID != traceID): # if the previous ID is different from the new one, it means we are in a different trace
         current_traceID = traceID # and consequently, we need to start by parsing the controller first
 
         controller_and_method = getControllerAndMethod(split_trace)
-        print("Controller.method: " + controller_and_method)
+        myPrint("Controller.method: " + controller_and_method)
 
         file_content[controller_and_method] = []
 
@@ -77,32 +91,49 @@ def parseExecutionTrace(trace: str):
     else: # if traceID hasn't changed, then we must continue to parse it and add the result to the current controller_and_method
         class_name, method = getClassNameAndMethod(split_trace)
 
-        if ("_Base" not in class_name):
+        if (class_name == "FenixFramework"): 
+            pass # FIXME check with samuel or rito to see if this is considered a read because we're already accessing the DomainRoot_Base
+        elif ("_Base" not in class_name):
             return
 
-        accessed_entity, access_type = parseMethod(method)
-        print("accessed_entity: " + accessed_entity)
-        print("access_type: " + access_type)
+        accessed_entity, access_type = parseMethod(class_name, method) # passing class_name cuz e.g, the getAcronym comes form the Edition(_Base) entity
+        myPrint("Accessed entity: " + accessed_entity)
+        myPrint("Access type: " + access_type)
 
         file_content[current_controller_and_method].append([accessed_entity, access_type])
-        
+
+def checkFileExists(file_dir: str):
+    if path.isfile(file_dir):
+        return file_dir
+    else:
+        exit("[ERROR]: The file " + file_dir + " you entered does not exist")
+
+def checkDirectoryExists(dir_path: str):
+    if path.isdir(dir_path):
+        return dir_path
+    else:
+        exit("[ERROR]: The directory " + dir_path + " does not exist")
+
+
+def parseCommandLineArguments() -> Dict[str, object]:
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--inputdir", type=checkFileExists, required=True, help="directory of the file to be parsed", metavar="")
+    ap.add_argument("-o", "--outputdir", help="directory for the generated file", default="./", metavar="")
+    ap.add_argument("-v", "--verbose", action="store_true", help="logging enabler")
+    
+    args = vars(ap.parse_args())
+    return args
 
 if __name__ == "__main__":
-    command_line_args: List[str] = argv
-    command_line_args_len: int = len(command_line_args)
+    args = parseCommandLineArguments()
 
-    print("Number of arguments: " + str(command_line_args_len) + " arguments")
-    print("Arguments list: " + str(command_line_args))
+    input_file_dir: str = args["inputdir"]
+    output_file_dir: str = args["outputdir"]
+    verbosity = args["verbose"]
 
-    if (command_line_args_len < 2):
-        exit("[ERROR]: More than one command line argument was provided. Please pass the path to the file you want to parse");
+    myPrint(args)
 
-    file_dir: str = command_line_args[1]
-
-    if (not path.isfile(file_dir)):
-        exit("[ERROR]: The file you entered does not exist");
-
-    with open(file_dir) as f:
+    with open(input_file_dir) as f:
         for line in f:
             if line in ['\n', '\r\n']: # ignore blank lines
                 continue
@@ -110,4 +141,4 @@ if __name__ == "__main__":
             parseExecutionTrace(line)
 
     # deleteControllersWithNoAccesses()
-    print(json.dumps(file_content, indent = 2))
+    myPrint(json.dumps(file_content, indent = 2))
