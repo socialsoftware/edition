4d229b8d3860624fb228cc73e633b6dcfc5ce2d8
Raimundo <joao.raimundo.1982@gmail.com>
2022-07-02 22:28:12 +0100

    update

--- ldod-mfes/shell/shared/router/node_modules/happy-dom/lib/event/events/IStorageEventInit.d.ts
+++
@@ -1,8 +0,0 @@
-import IEventInit from '../IEventInit';
-import Storage from '../../storage/Storage';
-export default interface IStorageEventInit extends IEventInit {
-    key?: string;
-    newValue?: string;
-    oldValue?: string;
-    storageArea?: Storage;
-}
--- ldod-mfes/shell/shared/router/node_modules/happy-dom/lib/fetch/IHeadersInit.d.ts
+++
@@ -1,5 +0,0 @@
-import IHeaders from './IHeaders';
-declare type IHeadersInit = string[][] | {
-    [key: string]: string;
-} | IHeaders;
-export default IHeadersInit;
